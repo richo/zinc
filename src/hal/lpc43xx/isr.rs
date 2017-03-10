7e7c4e813b4ac088c72ac2b039264589201b4d18
Dominic Spill <dominicgs@gmail.com>
2017-03-09 23:56:58 -0700

    Fix up ISR list to match LPC43xx interrupts

--- src/hal/lpc43xx/isr.rs
+++ src/hal/lpc43xx/isr.rs
@@ -13,89 +13,125 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! ISR Data for k20
+//! ISR Data for lpc43xx
 
 use core::option::Option::{self, Some};
 
 extern {
-  fn isr_wdt();
+  fn isr_dac();
+  fn isr_m0app();
+  fn isr_dma();
+  fn isr_reserved();
+  fn isr_flasheeprom();
+  fn isr_enet();
+  fn isr_sdio();
+  fn isr_lcd();
+  fn isr_usb0();
+  fn isr_usb1();
+  fn isr_sctimer_pwm();
+  fn isr_ritimer();
   fn isr_timer_0();
   fn isr_timer_1();
   fn isr_timer_2();
   fn isr_timer_3();
+  fn isr_mcpwm();
+  fn isr_adc0();
+  fn isr_i2c_0();
+  fn isr_i2c_1();
+  fn isr_spi();
+  fn isr_adc1();
+  fn isr_ssp_0();
+  fn isr_ssp_1();
   fn isr_uart_0();
   fn isr_uart_1();
   fn isr_uart_2();
   fn isr_uart_3();
-  fn isr_pwm_1();
-  fn isr_i2c_0();
-  fn isr_i2c_1();
-  fn isr_i2c_2();
-  fn isr_spi();
-  fn isr_ssp_0();
-  fn isr_ssp_1();
-  fn isr_pll_0();
+  fn isr_i2s0();
+  fn isr_i2s1();
+  fn isr_spifi();
+  fn isr_sgpio();
+  fn isr_pin_int0();
+  fn isr_pin_int1();
+  fn isr_pin_int2();
+  fn isr_pin_int3();
+  fn isr_pin_int4();
+  fn isr_pin_int5();
+  fn isr_pin_int6();
+  fn isr_pin_int7();
+  fn isr_gint0();
+  fn isr_gint1();
+  fn isr_eventrouter();
+  fn isr_c_can1();
+  fn isr_reserved();
+  fn isr_adchs();
+  fn isr_atimer();
   fn isr_rtc();
-  fn isr_eint_0();
-  fn isr_eint_1();
-  fn isr_eint_2();
-  fn isr_eint_3();
-  fn isr_adc();
-  fn isr_bod();
-  fn isr_usb();
-  fn isr_can();
-  fn isr_dma();
-  fn isr_i2s();
-  fn isr_enet();
-  fn isr_rit();
-  fn isr_mcpwm();
+  fn isr_reserved();
+  fn isr_wwdt();
+  fn isr_m0sub();
+  fn isr_c_can0();
   fn isr_qei();
-  fn isr_pll_1();
-  fn isr_usb_activity();
-  fn isr_can_activity();
 }
 
 #[allow(non_upper_case_globals)]
-const ISRCount: usize = 35;
+const ISRCount: usize = 53;
 
 #[allow(non_upper_case_globals)]
 #[link_section=".isr_vector_nvic"]
 #[no_mangle]
 pub static NVICVectors: [Option<unsafe extern fn()>; ISRCount] = [
-  // s.a. lpc43xx user manual, table 50 (chapter 6.3)
-  Some(isr_wdt),
+  // s.a. lpc43xx user manual (v2.1), table 78 (section 9.6.1)
+  Some(isr_dac),
+  Some(isr_m0app),
+  Some(isr_dma),
+  Some(isr_reserved),
+  Some(isr_flasheeprom),
+  Some(isr_enet),
+  Some(isr_sdio),
+  Some(isr_lcd),
+  Some(isr_usb0),
+  Some(isr_usb1),
+  Some(isr_sctimer_pwm),
+  Some(isr_ritimer),
   Some(isr_timer_0),
   Some(isr_timer_1),
   Some(isr_timer_2),
   Some(isr_timer_3),
+  Some(isr_mcpwm),
+  Some(isr_adc0),
+  Some(isr_i2c_0),
+  Some(isr_i2c_1),
+  Some(isr_spi),
+  Some(isr_adc1),
+  Some(isr_ssp_0),
+  Some(isr_ssp_1),
   Some(isr_uart_0),
   Some(isr_uart_1),
   Some(isr_uart_2),
   Some(isr_uart_3),
-  Some(isr_pwm_1),
-  Some(isr_i2c_0),
-  Some(isr_i2c_1),
-  Some(isr_i2c_2),
-  Some(isr_spi),
-  Some(isr_ssp_0),
-  Some(isr_ssp_1),
-  Some(isr_pll_0),
+  Some(isr_i2s0),
+  Some(isr_i2s1),
+  Some(isr_spifi),
+  Some(isr_sgpio),
+  Some(isr_pin_int0),
+  Some(isr_pin_int1),
+  Some(isr_pin_int2),
+  Some(isr_pin_int3),
+  Some(isr_pin_int4),
+  Some(isr_pin_int5),
+  Some(isr_pin_int6),
+  Some(isr_pin_int7),
+  Some(isr_gint0),
+  Some(isr_gint1),
+  Some(isr_eventrouter),
+  Some(isr_c_can1),
+  Some(isr_reserved),
+  Some(isr_adchs),
+  Some(isr_atimer),
   Some(isr_rtc),
-  Some(isr_eint_0),
-  Some(isr_eint_1),
-  Some(isr_eint_2),
-  Some(isr_eint_3),
-  Some(isr_adc),
-  Some(isr_bod),
-  Some(isr_usb),
-  Some(isr_can),
-  Some(isr_dma),
-  Some(isr_i2s),
-  Some(isr_enet),
-  Some(isr_rit),
-  Some(isr_mcpwm),
+  Some(isr_reserved),
+  Some(isr_wwdt),
+  Some(isr_m0sub),
+  Some(isr_c_can0),
   Some(isr_qei),
-  Some(isr_pll_1),
-  Some(isr_usb_activity),
-  Some(isr_can_activity),
 ];
